e50ef00b233fb71df4b36f60bb1b25553f8ac93c
gulewinner <workinegulelat@gmail.com>
2023-06-22 01:30:46 -0700

    Multiple mains

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /**
  * main - 4-puts.c
- * description - Write a C program that prints exactly
- *
+ * Description: A program that prints a specific message
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-
-        return (0);
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
-
